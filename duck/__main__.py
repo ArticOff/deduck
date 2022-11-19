4b9a5b94b2036f14307569695e0d42dca2646773
Artic <artic.admisoffi@gmail.com>
2022-11-19 19:45:07 +0100

    Add files via upload

---
+++ duck/__main__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+The MIT License (MIT)
+
+Copyright (c) 2022-present Artic
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+duck: str = """                                                                                                            
+               :^^~~~~~^^.                                  
+            .^!!!!!777777!~^.                               
+           .~!!~^^^~!777777!:                               
+           :!7!!~~~!!7777777:                               
+   :^:.  .^7Y?77!!!7777777!!:                               
+  :YBBGGPPGBBGG5YJ?!!77777~:                                
+   ~?YPGGGGGGGGGP5?!!777!^:                                 
+     .:^^^^^^^^77!!7777~^                                   
+              .!77777!~:                                    
+             :~!7777!^           .::^^~~~~^^^::.            
+            .~77!!!!!^      :~!7?JY55PPPPPPP55YJ?7~:        
+           :!777?JPGG?  .~7Y55PPPPPPPPPPPPPPPPPPPP55Y7~.    
+         .7YPGB&&&&#B5JY5PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP5?~. 
+        .!#@&&#BGP555PPPPPPPPPPPPPPPPPPPPPPP5555YYYYJJJJJJ!~
+        ^?5P5YYJJJYPPPPPPPPPPPPPPPPPPPPGGBBGGPP5YJ7!^^^^^^!?
+        ~?JJJJJJY5PPPPPPPPPPPPPPPGBB###############BG?~^^^~^
+        ~?JJJJJY5PPPPPPPPPPPGGB###&&#############&&&&#P!^!: 
+        .~?JJJJ5PPPPPPPPGBB##&&####################&##G7:   
+         .7JJJYPPPPPPGB##&&&######################&&#P7:    
+          :~JY5PPPGB##&&######################&&&&#P?:      
+           .^!?YPB#&&&&&&&&&&&&&&&&&&&&&&&&&&&#B5?~.        
+              .:^~~!!!!!!!!!!!!!!!!!!!!!!!!!!~~^.           
+            
+            Hi! explain your problem to me... quack! quack!
+"""
+
+print(duck)
